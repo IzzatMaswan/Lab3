260d618d57ba3215751a709378ae27003ec24287
IzzatMaswan <2020866462@student.uitm.edu.my>
2022-05-11 07:23:24 +0800

    firstcomment

---
+++ comment.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php
+// This is comment 
+// echo "X equals $x";
+$x += 10; // increment $x by 10 
+echo $x;
+/*This is a section
+of multiline comments 
+that will not be 
+interupted */
+?>
+</body>
+</html>
