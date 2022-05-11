03c4c8f3967440f09a1c8b858901dae2e095f8c4
IzzatMaswan <2020866462@student.uitm.edu.my>
2022-05-11 08:02:55 +0800

    last3

---
+++ string.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<?php
+$txt1 = "Hello there, World!";
+$txt2 = "Have a nice day!";
+$greet = $txt1 . " " . $txt2
+echo $greet;?>
+echo "<br />";
+echo strlen("Hello World");
+echo "<br />";
+echo strpos("Hello World!","World")
+echo "<br />";
+?>
+</body>
+</html>
